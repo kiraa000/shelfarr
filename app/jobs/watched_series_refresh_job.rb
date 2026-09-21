23b7cfbe7c1de4e7d2984668aaf1aafd802e77b3
kiraa000 <124541772+kiraa000@users.noreply.github.com>
2026-09-20 22:35:27 -0400

    Add recurring watched series refresh job

---
+++ app/jobs/watched_series_refresh_job.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class WatchedSeriesRefreshJob < ApplicationJob
+  queue_as :default
+
+  retry_on HardcoverClient::RateLimitError, wait: :polynomially_longer, attempts: 3
+
+  def perform
+    WatchedSeriesRegistrationService.backfill_existing!
+
+    WatchedSeries.enabled.includes(:user).find_each do |watched|
+      next if watched.user.deleted?
+
+      begin
+        result = WatchedSeriesRefreshService.call(watched)
+        Rails.logger.info(
+          "[WatchedSeriesRefreshJob] #{watched.title} (#{watched.collection_id}): "           "created=#{result.created_requests.size} skipped=#{result.skipped_items} errors=#{result.errors.size}"
+        )
+      rescue HardcoverClient::RateLimitError
+        raise
+      rescue StandardError => e
+        Rails.logger.warn(
+          "[WatchedSeriesRefreshJob] #{watched.title} (#{watched.collection_id}) failed: #{e.class}: #{e.message}"
+        )
+      end
+    end
+  end
+end
