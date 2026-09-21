6b3f2f896f9f0a7736d6c1348c56db1552f688c2
kiraa000 <124541772+kiraa000@users.noreply.github.com>
2026-09-20 21:58:36 -0400

    Test Decypharr provider routing

--- test/services/download_clients/decypharr_test.rb
+++ test/services/download_clients/decypharr_test.rb
@@ -16,6 +16,7 @@ class DownloadClients::DecypharrTest < ActiveSupport::TestCase
     @client = @client_record.adapter
 
     Thread.current[:qbittorrent_sessions] = {}
+    Setting.where(key: "decypharr_debrid_provider").delete_all
   end
 
   test "authenticates with lowercase sid cookie when generic API key is stored" do
@@ -121,6 +122,38 @@ class DownloadClients::DecypharrTest < ActiveSupport::TestCase
       result = @client.add_torrent("http://prowlarr:9696/api/v1/indexer/download/123")
 
       assert_equal expected_hash, result
+      assert_requested(add_stub)
+    end
+  end
+
+  test "sends configured Decypharr debrid provider with torrent submissions" do
+    VCR.turned_off do
+      SettingsService.set(:decypharr_debrid_provider, "realdebrid")
+
+      stub_request(:post, "http://localhost:8282/api/v2/auth/login")
+        .to_return(
+          status: 200,
+          headers: { "Set-Cookie" => "sid=test_session_id; path=/" },
+          body: "Ok."
+        )
+
+      add_stub = stub_request(:post, "http://localhost:8282/api/v2/torrents/add")
+        .with do |request|
+          params = URI.decode_www_form(request.body).to_h
+          params["debrid"] == "realdebrid"
+        end
+        .to_return(status: 200, body: "Ok.")
+
+      stub_request(:get, "http://localhost:8282/api/v2/torrents/info?hashes=a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2")
+        .to_return(
+          status: 200,
+          headers: { "Content-Type" => "application/json" },
+          body: [ { "hash" => "a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2", "name" => "Test", "progress" => 0, "state" => "downloading", "size" => 100, "content_path" => "/downloads" } ].to_json
+        )
+
+      result = @client.add_torrent("magnet:?xt=urn:btih:a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2")
+
+      assert_equal "a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2", result
       assert_requested(add_stub)
     end
   end
