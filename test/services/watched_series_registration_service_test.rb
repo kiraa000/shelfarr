8094e8694955e91c198fc97b44147a8d67863160
kiraa000 <124541772+kiraa000@users.noreply.github.com>
2026-09-20 22:40:39 -0400

    Test disabled watched series backfill safety

--- test/services/watched_series_registration_service_test.rb
+++ test/services/watched_series_registration_service_test.rb
@@ -30,6 +30,35 @@ class WatchedSeriesRegistrationServiceTest < ActiveSupport::TestCase
     assert watched.enabled?
   end
 
+  test "backfill does not re-enable a disabled watch" do
+    watched = WatchedSeries.create!(
+      user: @user,
+      collection_source: "hardcover",
+      collection_id: "987",
+      title: "Test Series",
+      book_types: [ "audiobook" ],
+      enabled: false
+    )
+    book = Book.create!(
+      title: "Existing Series Book",
+      book_type: :audiobook,
+      hardcover_id: "123"
+    )
+    Request.create!(
+      user: @user,
+      book: book,
+      status: :completed,
+      request_scope: "collection",
+      collection_source: "hardcover",
+      collection_id: "987",
+      collection_title: "Test Series"
+    )
+
+    WatchedSeriesRegistrationService.backfill_existing!
+
+    assert_not watched.reload.enabled?
+  end
+
   test "does not watch non-Hardcover collections" do
     assert_no_difference "WatchedSeries.count" do
       result = WatchedSeriesRegistrationService.register!(
